64baa08556bc12eeaa3cd5109f572114923ead95
Pablo Sagastizabal <sgszpablo@gmail.com>
2024-02-27 17:44:06 -0300

    Add unit test create mode case

--- ejercicio_1/components/LocationForm/LocationForm.test.tsx
+++ ejercicio_1/components/LocationForm/LocationForm.test.tsx
@@ -42,4 +42,32 @@ describe('LocationForm', () => {
     expect(latitudeInput).toHaveValue(location.latitude);
     expect(longitudeInput).toHaveValue(location.longitude);
   });
+  it('render the form in create mode', () => {
+    const countries = [
+      {
+        id: 1,
+        name: 'Argentina',
+      },
+      {
+        id: 2,
+        name: 'Francia',
+      },
+    ];
+    render(<LocationForm countries={countries} />);
+
+    const nameInput = screen.getByLabelText('Name');
+    const countrySelect = screen.getByLabelText('Select country');
+    const latitudeInput = screen.getByLabelText('Latitude');
+    const longitudeInput = screen.getByLabelText('Longitude');
+
+    expect(nameInput).toBeInTheDocument();
+    expect(countrySelect).toBeInTheDocument();
+    expect(latitudeInput).toBeInTheDocument();
+    expect(longitudeInput).toBeInTheDocument();
+
+    expect(nameInput).toHaveValue("");
+    expect(countrySelect).toHaveValue(countries[0].id.toString());
+    expect(latitudeInput).toHaveValue("");
+    expect(longitudeInput).toHaveValue("");
+  });
 });
